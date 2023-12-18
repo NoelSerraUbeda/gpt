255c3f178f3bc092d2fc3f0ff7c52ceb0a3c61d7
NoelSerraUbeda <neweonj@gmail.com>
2023-12-18 13:33:39 +0100

    Depuración e inhabilitar texto si se está reproduciendo parrafos

--- js/user-interaction.js
+++ js/user-interaction.js
@@ -2,58 +2,52 @@ class Message extends HTMLElement {
     constructor() {
         super();
         this.shadow = this.attachShadow({ mode: 'open' });
-
-        document.addEventListener('newChat', (event) => {
-            this.render();
-        });
-
-        document.addEventListener('stop', event => this.handleStop(event));
-        document.addEventListener('reStart', event => this.handlereStart(event));
-        document.addEventListener('returnSvg', event => this.handleReturnSvg(event));
+        this.isStopEventRunning = false;
+        
+        document.addEventListener('newChat', () => this.render());
+        document.addEventListener('stop', () => this.handleStop());
+        document.addEventListener('reStart', () => this.handleSvgChange());
+        document.addEventListener('returnSvg', () => this.handleSvgChange());
     }
 
+    // Evento de detener, deshabilitar y actualizar SVG
     handleStop = () => {
         this.isStopEventRunning = true;
 
+        // Deshabilitar textarea
+        const textarea = this.shadow.querySelector('.form-element textarea');
+        textarea.disabled = true;
+
+        // Habilitar durante stop
         const sendButton = this.shadow.querySelector('.send-button button');
         sendButton.disabled = false;
 
-        const originalClickHandler = sendButton.onclick;
-
-        sendButton.onclick = (event) => {
+        // Asignar controlador
+        sendButton.onclick = () => {
             if (this.isStopEventRunning) {
                 document.dispatchEvent(new CustomEvent('stopText'));
-            } else {
-                originalClickHandler && originalClickHandler(event);
             }
         };
-
-        const buttonSvg = this.shadow.querySelector('.send-button button svg');
-        buttonSvg.innerHTML = '<svg fill="white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>stop-circle-outline</title><path d="M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4M9,9V15H15V9" /></svg>';
-
+        // Cambiar SVG a stop.
+        this.updateSvg('<svg fill="white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>stop-circle-outline</title><path d="M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4M9,9V15H15V9" /></svg>');
     };
 
-
-    handlereStart = () => {
+    // Devolver SVG original
+    handleSvgChange = () => {
+        // recuperar textarea
         const textarea = this.shadow.querySelector('.form-element textarea');
-        const sendButton = this.shadow.querySelector('.send-button button');
-
-        const buttonSvg = this.shadow.querySelector('.send-button button svg');
-        buttonSvg.innerHTML = '<svg viewBox="0 0 24 24" fill="none" class="text-white dark:text-black"><path d="M7 11L12 6L17 11M12 18V7" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>';
-
-        sendButton.onclick = this.originalSendButtonClickHandler;
+        textarea.disabled = false;
+        this.updateSvg('<svg viewBox="0 0 24 24" fill="none" class="text-white dark:text-black"><path d="M7 11L12 6L17 11M12 18V7" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>');
     }
 
-    handleReturnSvg = () => {
+    // Actualiza SVG
+    updateSvg = (svgString) => {
         const buttonSvg = this.shadow.querySelector('.send-button button svg');
-        buttonSvg.innerHTML = '<svg viewBox="0 0 24 24" fill="none" class="text-white dark:text-black"><path d="M7 11L12 6L17 11M12 18V7" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>';
-    }
+        buttonSvg.innerHTML = svgString;
+    };
 
     connectedCallback() {
         this.render();
-
-        const sendButton = this.shadow.querySelector('.send-button button');
-        this.originalSendButtonClickHandler = sendButton.onclick;
     }
 
     render() {
